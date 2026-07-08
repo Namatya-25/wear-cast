f9c4dd7a6c146eb45799e930d9a5795d8996fd6a
namatya <langhuanamacha@gmail.com>
2026-07-08 12:48:20 +0900

    dbとserverファイルの変更

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
 const express = require("express");
 const app = express();
+const db = require("./src/config/db.js");
 const port = 3000;
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("hello")
+    res.send("サーバーは正常に稼働中です")
 });
 
 
 app.listen(port, () => {
-    console.log("サーバ起動中")
-})
+    console.log(`サーバがポート ${port} で起動しました`)
+});
+
+//データを登録する機能 多分仮
+app.post("/add-clothes", (req, res) => {
+    const {name, category} = req.body;
+    const sql = "INSERT INTO clothes (name, category) VALUES (?, ?)";
+
+    db.query(sql, [name, category], (err, result) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send("保存に失敗しました");
+        }
+        res.send("服を登録しました");
+    });
+});
--- src/config/db.js
+++ src/config/db.js
@@ -4,11 +4,14 @@ const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
-    database: ""
+    database: "wear_cast_db"
 });
 
 connection.connect((err) => {
-    if (err) throw err;
+    if (err) {
+        console.error("データベース接続失敗: " + err.stack);
+        return;
+    }
     console.log("データベースに接続しました");
 });
 
